c0912e34fc643bb5886892012c1b8f755e9b78b3
jhacasky <103072608+Jhacasky52@users.noreply.github.com>
2025-10-15 16:40:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,42 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
-}
+#include "funciones.h"
+int main() {
+    std::string nombreArchivo;
+    int n, metodo;
+
+    std::cout << "Nombre del archivo de entrada: ";
+    std::cin >> nombreArchivo;
+
+    std::cout << "Ingresa la semilla (n): ";
+    std::cin >> n;
+
+    std::cout << "Selecciona el metodo de codificacion (1): ";
+    std::cin >> metodo;
+
+    if (metodo != 1) {
+        std::cerr << "Metodo no implementado." << std::endl;
+        return 1;
+    }
+
+    std::vector<std::string> bloques = codificarArchivo("C:/Users/Est01/Desktop/jhacasky/practica4/practica3/ABCD.txt", n);
+
+    if (bloques.empty()) {
+        std::cerr << "Error durante la codificacion." << std::endl;
+        return 1;
+    }
+
+    std::cout << "\nBloques codificados de " << n << " bits:\n";
+    for (const auto& bloque : bloques) {
+        std::cout << bloque << " ";
+    }
+    std::cout << std::endl;
+    std::vector<std::string> bloquesCodificados = transformarBloques(bloques);
+    std::cout << "Bloques codificados:\n";
+    string bloque="1101";
+    cout<<invertirBitEspecificoEnCadaGrupo(bloque,3,3)<<""<<endl;
+    for (const auto& bloque : bloquesCodificados) {
+        std::cout << bloque << " ";
+    }
+    std::cout << std::endl;
